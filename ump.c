75761db01a182ffdfd4f2335a9eb77958e629dda
virtualgrub39 <virtualgrub39@tutamail.com>
2025-05-18 19:56:37 +0200

    introduced get_file_info()

--- ump.c
+++ ump.c
@@ -5,6 +5,54 @@
 #include <miniaudio.h>
 #include <taglib/tag_c.h>
 
+struct file_info {
+    const char* file_path;
+    const char* title;
+    const char* artist;
+    const char* album;
+    const char* genre;
+    unsigned int year;
+    int bitrate_kbps;
+    int length_secs;
+    int channels;
+    int samplerate_hz;
+};
+
+bool
+get_file_info(const char* path, struct file_info* info)
+{
+    TagLib_File* file = taglib_file_new(path);
+    if (!file) {
+        perror("taglib_file_new");
+        return false;
+    }
+    TagLib_Tag* tag = taglib_file_tag(file);
+    if (!tag) {
+        fprintf(stderr, "No tags in %s\n", path);
+        taglib_file_free(file);
+        return false;
+    }
+    const TagLib_AudioProperties* ap = taglib_file_audioproperties(file);
+    if (!ap) {
+        fprintf(stderr, "Couldn't read audio properties of %s\n", path);
+        taglib_file_free(file);
+        return false;
+    }
+
+    info->title  = taglib_tag_title(tag);
+    info->artist = taglib_tag_artist(tag);
+    info->album  = taglib_tag_album(tag);
+    info->year  = taglib_tag_year(tag);
+    info->genre  = taglib_tag_genre(tag);
+    info->bitrate_kbps = taglib_audioproperties_bitrate(ap);
+    info->length_secs = taglib_audioproperties_length(ap);
+    info->channels = taglib_audioproperties_channels(ap);
+    info->samplerate_hz = taglib_audioproperties_samplerate(ap);
+
+    taglib_file_free(file);
+    return true;
+}
+
 int
 main(int argc, char* argv[])
 {
@@ -18,37 +66,25 @@ main(int argc, char* argv[])
         return 1;
     }
 
-    TagLib_File *file = taglib_file_new(argv[1]);
-    if (!file) {
-        perror("taglib_file_new");
-        return 1;
-    }
-    TagLib_Tag *tag = taglib_file_tag(file);
-    if (!tag) {
-        fprintf(stderr, "No tags in %s\n", argv[1]);
-        taglib_file_free(file);
-        return 0;
-    }
-
-    const char *title  = taglib_tag_title(tag);
-    const char *artist = taglib_tag_artist(tag);
-    const char *album  = taglib_tag_album(tag);
-    unsigned int year  = taglib_tag_year(tag);
-    const char *genre  = taglib_tag_genre(tag);
+    struct file_info info = { 0 };
+    get_file_info(argv[1], &info);
 
     initscr();
     cbreak();
     noecho();
 
-    mvprintw(1, 1, "Title : %s",  title);
-    mvprintw(2, 1, "Artist: %s",  artist);
-    mvprintw(3, 1, "Album : %s",  album);
-    mvprintw(4, 1, "Year  : %u",  year);
-    mvprintw(5, 1, "Genre : %s",  genre);
+    mvprintw(1, 1, "Title     : %s",  info.title);
+    mvprintw(2, 1, "Artist    : %s",  info.artist);
+    mvprintw(3, 1, "Album     : %s",  info.album);
+    mvprintw(4, 1, "Year      : %u",  info.year);
+    mvprintw(5, 1, "Genre     : %s",  info.genre);
+    mvprintw(6, 1, "Bitrate   : %u [kb/s]",  info.bitrate_kbps);
+    mvprintw(7, 1, "Length    : %u [s]",  info.length_secs);
+    mvprintw(8, 1, "Channels  : %u",  info.channels);
+    mvprintw(9, 1, "Samplerate: %u [hz]",  info.samplerate_hz);
     refresh();
 
     taglib_tag_free_strings();
-    taglib_file_free(file);
 
     ma_engine engine;
     if (ma_engine_init(NULL, &engine) != MA_SUCCESS) {
